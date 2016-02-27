5affd91cbbb552cb6e0785bede66949858e762ac
Martin Blackburn <walking_fishy@hotmail.com>
2016-02-27 12:12:48 +0000

    added id to items, updated test for name, created test for same id

--- assets/js/tests.js
+++ assets/js/tests.js
@@ -31,7 +31,8 @@ App.Tests = (function()
         categories = $(".category");
         items = $(".item");
 
-        sameCategoryId();
+        sameCategoryName();
+        sameItemName();
         sameItemId();
         prerequisitesExist();
         prerequisiteNotCategory();
@@ -43,17 +44,17 @@ App.Tests = (function()
 
 
     /**
-     * Make sure no category has the same id
+     * Make sure no category has the same name
      */
-    function sameCategoryId()
+    function sameCategoryName()
     {
         categories.each(function() {
-            var id = $(this).data("id").toString();
+            var name = $(this).data("name").toString();
 
-            var sameCategoryId = container.find("[data-id='" + id + "']");
+            var sameCategoryId = container.find("[data-name='" + name + "']");
 
             if(sameCategoryId.length > 1) {
-                console.error("There are categories with the same id: " + id);
+                console.error("There are categories with the same name: " + name);
             }
         });
     }
@@ -69,11 +70,32 @@ App.Tests = (function()
     {
         items.each(function() {
             var id = $(this).data("id").toString();
+            var name = $(this).data("name").toString();
 
             var sameItemId = container.find("[data-id='" + id + "']");
 
             if(sameItemId.length > 1) {
-                console.error("There are items with the same id: " + id);
+                console.error("There are items with the same id: " + name + " (" + id + ")");
+            }
+        });
+    }
+
+
+
+
+
+    /**
+     * Make sure no item has the same name
+     */
+    function sameItemName()
+    {
+        items.each(function() {
+            var name = $(this).data("name").toString();
+
+            var sameItemId = container.find("[data-name='" + name + "']");
+
+            if(sameItemId.length > 1) {
+                console.error("There are items with the same name: " + name);
             }
         });
     }
@@ -97,11 +119,11 @@ App.Tests = (function()
 
                 //check each prerequisite
                 for(var i = 0; i < prerequisitesArray.length; i++) {
-                    var requiredItem = container.find("[data-id='" + prerequisitesArray[i] + "']");
+                    var requiredItem = container.find("[data-name='" + prerequisitesArray[i] + "']");
 
                     //check item exists
                     if(requiredItem.length < 1) {
-                        console.error("Item '" + $(this).data("id") + "' doesn't have prerequisite '" + prerequisitesArray[i] + "'");
+                        console.error("Item '" + $(this).data("name") + "' doesn't have prerequisite '" + prerequisitesArray[i] + "'");
                     }
                 }
             }
@@ -113,7 +135,7 @@ App.Tests = (function()
 
 
     /**
-     * Make sure prerequisites isn't a category
+     * Make sure prerequisite isn't a category
      */
     function prerequisiteNotCategory()
     {
@@ -127,11 +149,11 @@ App.Tests = (function()
 
                 //check each prerequisite
                 for(var i = 0; i < prerequisitesArray.length; i++) {
-                    var requiredItem = container.find("[data-id='" + prerequisitesArray[i] + "']");
+                    var requiredItem = container.find("[data-name='" + prerequisitesArray[i] + "']");
 
                     //check prerequisite isn't a category
                     if(requiredItem.hasClass("category")) {
-                        console.error("Item '" + $(this).data("id") + "' has prerequisite which is a category '" + prerequisitesArray[i] + "'");
+                        console.error("Item '" + $(this).data("name") + "' has prerequisite which is a category '" + prerequisitesArray[i] + "'");
                     }
                 }
             }
@@ -149,10 +171,10 @@ App.Tests = (function()
     {
         categories.each(function() {
             var childItems = $(this).find(".item");
-            var id = $(this).data("id").toString();
+            var name = $(this).data("name").toString();
 
             if(childItems.length === 0) {
-                console.error("Categories has no items: " + id);
+                console.error("Categories has no items: " + name);
             }
         });
     }
