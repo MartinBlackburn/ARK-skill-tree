5c3a57c0706a38b35adb1e22bd6d06fac309195b
Martin Blackburn <martin.blackburn@skybettingandgaming.com>
2016-07-01 20:19:27 +0100

    bumped version to 243

--- assets/js/items.js
+++ assets/js/items.js
@@ -5,7 +5,7 @@ App.Items = (function()
     //variables
     var container = $(".js-categories");
     var useLocalStorage = false;
-    var version = "242.7";
+    var version = "243";
     var highestItemId = 0;
     var totalEngramPoints = 0;
 
